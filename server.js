842c3b9533e5a21972f200874da2254b887358b3
s1ammy <maksimsavincev@gmail.com>
2023-06-22 15:08:11 +0300

    feat: 404 error handling

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ app.listen(port, console.log(`Server listening port ${port}`));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.static('public'));
 
+app.get('*', (req, res) => {
+    res.sendStatus(404);
+});
+
 // POST method to request WPM
 app.post('/wpm', (req, res) => {
-    insertWpm(req.body.data);
+    insertWpm(req.body.wpm);
     res.end();
 });
